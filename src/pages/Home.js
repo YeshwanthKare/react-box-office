370af7b9a05239d7d8954a6612caf17f8b24117d
YeshwanthKare <yesh.kare@gmail.com>
2021-08-10 17:20:20 +0530

    ADD: added navigation for pages

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+const Home = () => (
+  <Route exact path="/">
+    This is home page
+  </Route>
+);
+
+export default Home;
---
+++ src/pages/Starred.js
@@ -0,0 +1,6 @@
+import React from 'react';
+// import { Route } from 'react-router-dom';
+
+const Starred = () => <div>This is starred Page</div>;
+
+export default Starred;
