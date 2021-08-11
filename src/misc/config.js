c6ee1586a6f76a27164d29e64b64e5c02b98ed0b
YeshwanthKare <yesh.kare@gmail.com>
2021-08-11 16:59:51 +0530

    ADD: added the fetch with async await

---
+++ src/misc/config.js
@@ -0,0 +1,6 @@
+const API_BASE_URL = `https://api.tvmaze.com`;
+
+export async function getURL(queryString) {
+  const response = await fetch(`${API_BASE_URL}${queryString}`);
+  return response;
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,33 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { getURL } from '../misc/config';
 
 const Home = () => {
   const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
 
   const onSearch = () => {
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+    getURL(`/search/shows?q=${input}`)
       .then(r => r.json())
-      .then(r => console.log(r));
+      .then(r => setResults(r));
   };
 
   const onKeyCode = e => {
     if (e.keyCode === 13) {
       onSearch();
     }
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No Results</div>;
+    }
+    if (results && results.length > 0) {
+      return results.map(item => (
+        <div key={item.show.id}>{item.show.name}</div>
+      ));
+    }
+    return null;
   };
 
   const onChnageKey = e => {
@@ -30,6 +44,7 @@ const Home = () => {
       <button type="button" disabled={!input} onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
