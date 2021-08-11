2c4389310a36d25c0096d35390bd78f144490a22
YeshwanthKare <yesh.kare@gmail.com>
2021-08-11 21:39:14 +0530

    ADD: added search option

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,12 +5,16 @@ import { getURL } from '../misc/config';
 const Home = () => {
   const [input, setInput] = useState('');
   const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
+  const isShowSearch = searchOption === 'shows';
 
   const onSearch = () => {
-    getURL(`/search/shows?q=${input}`)
+    getURL(`/search/${searchOption}?q=${input}`)
       .then(r => r.json())
       .then(r => setResults(r));
   };
+
+  console.log(results);
 
   const onKeyCode = e => {
     if (e.keyCode === 13) {
@@ -23,11 +27,22 @@ const Home = () => {
       return <div>No Results</div>;
     }
     if (results && results.length > 0) {
-      return results.map(item => (
-        <div key={item.show.id}>{item.show.name}</div>
-      ));
+      if (results[0].show) {
+        return results.map(item => (
+          <div key={item.show.id}>{item.show.name}</div>
+        ));
+      }
+      if (results[0].person) {
+        return results.map(item => (
+          <div key={item.person.id}>{item.person.name}</div>
+        ));
+      }
     }
     return null;
+  };
+
+  const changeRadioInput = e => {
+    setSearchOption(e.target.value);
   };
 
   const onChnageKey = e => {
@@ -38,9 +53,32 @@ const Home = () => {
       <input
         type="text"
         onChange={onChnageKey}
-        value={input}
+        placeholder="Search for something"
         onKeyDown={onKeyCode}
       />
+
+      <label htmlFor="show-search">
+        Shows
+        <input
+          id="show-search"
+          type="radio"
+          value="shows"
+          checked={isShowSearch}
+          onChange={changeRadioInput}
+        />
+      </label>
+
+      <label htmlFor="actor-search">
+        Actors
+        <input
+          id="actor-search"
+          type="radio"
+          value="people"
+          checked={!isShowSearch}
+          onChange={changeRadioInput}
+        />
+      </label>
+
       <button type="button" disabled={!input} onClick={onSearch}>
         Search
       </button>
