d8bc8bbc7c822f5a9a1a928bd5ecdae72e25dd09
YeshwanthKare <yesh.kare@gmail.com>
2021-08-13 17:06:16 +0530

    ADD: added error and loaded message to show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -5,14 +5,41 @@ import { getURL } from '../misc/config';
 const Show = () => {
   const { id } = useParams();
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    let isMounted = true;
+
     getURL(`/shows/${id}?embed[]=seasons&embed[]=cast`)
       .then(r => r.json())
-      .then(res => setShow(res));
+      .then(res => {
+        if (isMounted) {
+          setShow(res);
+          setIsLoading(false);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
 
   console.log(show);
+
+  if (isLoading) {
+    return <div>The page is being loaded</div>;
+  }
+
+  if (error) {
+    return <div>error occured: {error}</div>;
+  }
 
   return <div>this is show page</div>;
 };
